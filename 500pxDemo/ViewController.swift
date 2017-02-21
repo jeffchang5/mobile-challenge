3900d6eb2aeef04baf5a2b88ead1ab10b36876fa
Jeffrey Chang <jeffreychang@jeffreys-macbook-pro.local>
2017-02-20 20:45:37 -0500

    Project setup

---
+++ 500pxDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  500pxDemo
+//
+//  Created by Jeffrey Chang on 2/20/17.
+//  Copyright © 2017 Jeffrey Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
